6b0588a09b80e20b17305b1db262dbc262862f4e
v9rewinds <kenpokid14@gmail.com>
2018-05-03 15:44:31 -0400

    Ficing Jeriah's Problems

--- Orion-Entertainment.Malden/config/Config_Weapons.hpp
+++ Orion-Entertainment.Malden/config/Config_Weapons.hpp
@@ -54,22 +54,22 @@ class WeaponShops {
 			{ "arifle_Katiba_F", "", 70000, -1, "mav_ttm_var_rebel > 2" }, //Katiba
 			{ "srifle_DMR_07_blk_F", "", 80000, -1, "mav_ttm_var_rebel > 2" }, //CMR-76
 			{ "arifle_ARX_blk_F", "", 80000, -1, "mav_ttm_var_rebel > 3" }, //Type 115
-			{ "arifle_M9XM_Black_F", "", 90000, -1, "mav_ttm_var_rebel > 3" }, //MXM Black
+			{ "arifle_MXM_F", "", 90000, -1, "mav_ttm_var_rebel > 3" }, //MXM
 			{ "arifle_SPAR_02_blk_F", "", 120000, -1, "mav_ttm_var_rebel > 3" }, //Spar-16
 			{ "LMG_03_F", "", 150000, -1, "mav_ttm_var_rebel > 3" }, //Lim-85
-			{ "arifle_CTARS_blk_F", "", 150000, -1, "mav_ttm_var_rebel > 3" }, //Car-95-1
 			{ "arifle_AKM_F", "", 100000, -1, "mav_ttm_var_rebel > 3" }, //AKM
 			{ "srifle_DMR_01_F", "", 150000, -1, "mav_ttm_var_rebel > 3" }, //Rehim
 			{ "srifle_DMR_03_F", "", 200000, -1, "mav_ttm_var_rebel > 4" }, //Mk-1 EMR
-			{ "srifle_EBR_F", "", 180000, -1, "mav_ttm_var_rebel > 4" }, //Mk18 ABR
 			{ "srifle_DMR_06_olive_F", "", 170000, -1, "mav_ttm_var_rebel > 4" }, //MK14
 			{ "arifle_SPAR_03_blk_F", "", 175000, -1, "mav_ttm_var_rebel > 4" }, //Spar-17
+			{ "arifle_CTARS_blk_F", "", 150000, -1, "mav_ttm_var_rebel > 4" }, //Car-95-1
 			{ "arifle_AK12_F", "", 175000, -1, "mav_ttm_var_rebel > 4" }, //AK-12
 			{ "LMG_Mk200_F", "", 300000, -1, "mav_ttm_var_rebel > 4" }, //MK200
 			{ "arifle_MX_SW_F", "", 200000, -1, "mav_ttm_var_rebel > 4" }, //MX SW
-			{ "srifle_DMR_05_blk_F", "", 600000, -1, "mav_ttm_var_rebel > 4" }, //Cyrus
-			{ "srifle_DMR_02_F", "", 500000, -1, "mav_ttm_var_rebel > 4" }, //MAR-10
-			{ "LMG_Zafir_F", "", 800000, -1, "mav_ttm_var_rebel > 4" } //Zafir	
+			{ "srifle_EBR_F", "", 180000, -1, "mav_ttm_var_rebel > 5" }, //Mk18 ABR
+			{ "srifle_DMR_05_tan_F", "", 600000, -1, "mav_ttm_var_rebel > 5" }, //Cyrus
+			{ "srifle_DMR_02_sniper_F	", "", 500000, -1, "mav_ttm_var_rebel > 5" }, //MAR-10
+			{ "LMG_Zafir_F", "", 800000, -1, "mav_ttm_var_rebel > 5" } //Zafir	
         };
         mags[] = {
             { "20Rnd_556x45_UW_mag", "", 100, 50, "" }, //sdar
@@ -91,7 +91,7 @@ class WeaponShops {
 			{ "20Rnd_650x39_Cased_Mag_F", "", 100, 50, "" }, //CMR-78
 			{ "30Rnd_65x39_caseless_green", "", 100, 50, "" }, //Type 115
 			{ "10Rnd_50BW_Mag_F", "", 50000, 50, "" }, //50 Cal Mag
-			{ "30Rnd_65x39_caseless_mag", "", 100, 50, "" }, // MXM Black
+			{ "30Rnd_65x39_caseless_mag", "", 100, 50, "" }, // MXM 
 			{ "150Rnd_556x45_Drum_Mag_F", "", 100, 50, "" }, //Spar-16s
 			{ "200Rnd_556x45_Box_F", "", 1000, 50, "" }, //Lim-85
 			{ "100Rnd_580x42_Mag_F", "", 100, 50, "" }, //Car-95-1
@@ -212,7 +212,6 @@ class WeaponShops {
         side = "civ";
         conditions = "";
         items[] = {
-            { "NVGoggles_OPFOR", "", 5000, 75, ""}, //Black NightVision
             { "Binocular", "", 150, 75, "" },      //Binocular
             { "ItemGPS", "", 100, 50, "" }       //GPS
         };
@@ -225,7 +224,6 @@ class WeaponShops {
         side = "";
         conditions = "";
         items[] = {
-            { "NVGoggles_OPFOR", "", 5000, 75, ""}, //Black NightVision
             { "Binocular", "", 150, 75, "" },      //Binocular
             { "ItemGPS", "", 100, 50, "" }       //GPS
         };
